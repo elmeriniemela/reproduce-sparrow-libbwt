ceb9cff99d20928c3011c72937c5d7e5c9424620
Elmeri Niemelä <niemela.elmeri@gmail.com>
2026-09-05 20:00:40 +0300

    Squashed 'libbwt-jni/' content from commit 6f945d4
    git-subtree-dir: libbwt-jni
    git-subtree-split: 6f945d4d58418fbb36cea03940b7613940b619a0

---
+++ library/src/main/java/dev/bwt/libbwt/daemon/CallbackNotifier.java
@@ -0,0 +1,11 @@
+package dev.bwt.libbwt.daemon;
+
+public interface CallbackNotifier {
+    void onBooting(long shutdownPtr);
+    void onSyncProgress(float progress, int tip);
+    void onScanProgress(float progress, int eta);
+    void onElectrumReady(String addr);
+    void onHttpReady(String addr);
+    void onAccessToken(String token);
+    void onReady();
+}
---
+++ src/lib.rs
@@ -0,0 +1,210 @@
+#[macro_use]
+extern crate log;
+
+use std::sync::{mpsc, Once};
+use std::{any, panic, thread};
+
+use jni::objects::{GlobalRef, JClass, JObject, JString};
+use jni::sys::{jfloat, jint, jlong};
+use jni::{JNIEnv, JavaVM};
+
+use bwt::error::{BwtError, Context, Error, Result};
+use bwt::util::on_oneshot_done;
+use bwt::{App, Config, Progress};
+
+static INIT_LOGGER: Once = Once::new();
+
+#[repr(C)]
+pub struct ShutdownHandler(mpsc::SyncSender<()>);
+
+#[no_mangle]
+pub extern "system" fn Java_dev_bwt_libbwt_daemon_NativeBwtDaemon_start(
+    env: JNIEnv,
+    _: JClass,
+    json_config: JString,
+    callback: JObject,
+) {
+    let json_config: String = env.get_string(json_config).unwrap().into();
+
+    let jvm = env.get_java_vm().unwrap();
+    let callback_g = env.new_global_ref(callback).unwrap();
+
+    let start = || -> Result<_> {
+        let config: Config = serde_json::from_str(&json_config).context("Invalid config")?;
+        // The verbosity level cannot be changed once enabled.
+        INIT_LOGGER.call_once(|| config.setup_logger());
+        let is_ephemeral_auth = config.auth_ephemeral;
+
+        // Spawn background thread to emit syncing/scanning progress updates
+        let (progress_tx, progress_rx) = mpsc::channel();
+        spawn_recv_progress_thread(progress_rx, jvm, callback_g);
+
+        // Setup shutdown channel and pass the shutdown handler to onBooting
+        let (shutdown_tx, shutdown_rx) = make_shutdown_channel(progress_tx.clone());
+        let shutdown_ptr = ShutdownHandler(shutdown_tx).into_raw() as jlong;
+        env.call_method(callback, "onBooting", "(J)V", &[shutdown_ptr.into()])
+            .unwrap();
+
+        // Start up bwt, run the initial sync and start the servers
+        let app = App::boot(config, Some(progress_tx))?;
+
+        if shutdown_rx.try_recv() != Err(mpsc::TryRecvError::Empty) {
+            return Err(BwtError::Canceled.into());
+        }
+
+        if is_ephemeral_auth {
+            let access_token = env
+                .new_string(app.access_token().unwrap())
+                .unwrap()
+                .into_inner();
+            env.call_method(
+                callback,
+                "onAccessToken",
+                "(Ljava/lang/String;)V",
+                &[access_token.into()],
+            )
+            .unwrap();
+        }
+
+        #[cfg(feature = "electrum")]
+        if let Some(addr) = app.electrum_addr() {
+            let addr = env.new_string(addr.to_string()).unwrap().into_inner();
+            env.call_method(
+                callback,
+                "onElectrumReady",
+                "(Ljava/lang/String;)V",
+                &[addr.into()],
+            )
+            .unwrap();
+        }
+        #[cfg(feature = "http")]
+        if let Some(addr) = app.http_addr() {
+            let addr = env.new_string(addr.to_string()).unwrap().into_inner();
+            env.call_method(
+                callback,
+                "onHttpReady",
+                "(Ljava/lang/String;)V",
+                &[addr.into()],
+            )
+            .unwrap();
+        }
+
+        env.call_method(callback, "onReady", "()V", &[]).unwrap();
+
+        app.sync_loop(Some(shutdown_rx));
+
+        Ok(())
+    };
+
+    if let Err(e) = try_run(start) {
+        warn!("{}", e);
+        env.throw_new("dev/bwt/libbwt/BwtException", &e).unwrap();
+    } else {
+        debug!("daemon stopped successfully");
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_bwt_libbwt_daemon_NativeBwtDaemon_shutdown(
+    _env: JNIEnv,
+    _: JClass,
+    shutdown_ptr: jlong,
+) {
+    // Take ownership and drop it. This will disconnect the mpsc channel and shutdown the app.
+    Box::from_raw(shutdown_ptr as *mut ShutdownHandler);
+}
+
+impl ShutdownHandler {
+    fn into_raw(self) -> *const ShutdownHandler {
+        Box::into_raw(Box::new(self))
+    }
+}
+
+// Run the closure, handling panics and errors, and formatting them to a string
+fn try_run<F>(f: F) -> std::result::Result<(), String>
+where
+    F: FnOnce() -> Result<()> + panic::UnwindSafe,
+{
+    match panic::catch_unwind(f) {
+        Err(panic) => Err(fmt_panic(panic)),
+        // Consider user-initiated cancellations (via `bwt_shutdown`) as successful termination
+        Ok(Err(e)) if matches!(e.downcast_ref::<BwtError>(), Some(BwtError::Canceled)) => Ok(()),
+        Ok(Err(e)) => Err(fmt_error(e)),
+        Ok(Ok(())) => Ok(()),
+    }
+}
+
+// Spawn a thread to receive mpsc progress updates and forward them to on{Sync,Scan}Progress
+fn spawn_recv_progress_thread(
+    progress_rx: mpsc::Receiver<Progress>,
+    jvm: JavaVM,
+    callback: GlobalRef,
+) -> thread::JoinHandle<()> {
+    let (tx, rx) = mpsc::sync_channel(1);
+    let handle = thread::spawn(move || {
+        tx.send(()).unwrap();
+        let env = jvm.attach_current_thread().unwrap();
+        loop {
+            match progress_rx.recv() {
+                Ok(Progress::Sync { progress_f, tip }) => {
+                    let progress_f = progress_f as jfloat;
+                    let tip = tip as jint;
+                    env.call_method(
+                        &callback,
+                        "onSyncProgress",
+                        "(FI)V",
+                        &[progress_f.into(), tip.into()],
+                    )
+                    .unwrap();
+                }
+                Ok(Progress::Scan { progress_f, eta }) => {
+                    let progress_f = progress_f as jfloat;
+                    let eta = eta as jint;
+                    env.call_method(
+                        &callback,
+                        "onScanProgress",
+                        "(FI)V",
+                        &[progress_f.into(), eta.into()],
+                    )
+                    .unwrap();
+                }
+                Err(mpsc::RecvError) | Ok(Progress::Done) => break,
+            }
+        }
+    });
+    // wait for the thread to start
+    rx.recv().unwrap();
+
+    handle
+}
+
+fn make_shutdown_channel(
+    progress_tx: mpsc::Sender<Progress>,
+) -> (mpsc::SyncSender<()>, mpsc::Receiver<()>) {
+    let (shutdown_tx, shutdown_rx) = mpsc::sync_channel(1);
+
+    // When the shutdown signal is received, we need to emit a Progress::Done
+    // message to stop the progress recv thread, which will disconnect the
+    // progress channel and stop the bwt start-up procedure.
+    let shutdown_rx = on_oneshot_done(shutdown_rx, move || {
+        debug!("shutdown signal received");
+        progress_tx.send(Progress::Done).ok();
+    });
+
+    (shutdown_tx, shutdown_rx)
+}
+
+fn fmt_error(err: Error) -> String {
+    let causes: Vec<String> = err.chain().map(|cause| cause.to_string()).collect();
+    causes.join(": ")
+}
+
+fn fmt_panic(err: Box<dyn any::Any + Send + 'static>) -> String {
+    if let Some(s) = err.downcast_ref::<&str>() {
+        s.to_string()
+    } else if let Some(s) = err.downcast_ref::<String>() {
+        s.to_string()
+    } else {
+        "unknown panic".to_string()
+    }
+}
